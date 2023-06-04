4eb443d1e2d730d1722706ac4bbec24b39da91c7
SPoluliakh <S_Poluliakh@ukr.net>
2023-06-04 22:31:14 +0300

    imlement project

--- README.md
+++
@@ -1,1 +0,0 @@
-# wow-clean-shine
---
+++ src/declaration.d.ts
@@ -0,0 +1,25 @@
+/// <reference types="react-scripts" />
+
+declare module '*.gif' {
+  const src: string;
+  export default src;
+}
+
+declare module 'react-ellipsis-text';
+
+declare module 'react-simple-star-rating';
+
+// declare module '*.css' ('*.scss') {
+//   const content: Record<string, string>;
+//   export default content;
+// }
+
+// declare module '*.svg' {
+//   const src: string;
+//   export default src;
+// }
+
+// declare module '*.gif' {
+//   const src: string;
+//   export default src;
+// }
